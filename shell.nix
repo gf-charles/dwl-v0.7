24189b4c976230da6ce294997a85d649e5358091
Charles Faisandier <faisandiercharles@outlook.com>
2025-07-13 17:31:13 -0700

    pre-patches

---
+++ shell.nix
@@ -0,0 +1,29 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    libglvnd
+    libinput
+    pixman
+    wayland
+    wayland-protocols
+    libdrm
+    libevdev
+    libxkbcommon
+    libudev-zero
+    libcap
+    wlroots
+    fcft
+  ];
+
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    wayland-scanner
+  ];
+
+  shellHook = ''
+    export SHELL=${pkgs.zsh}/bin/zsh
+    echo "Ready to compile dwl. "
+  '';
+}
+
